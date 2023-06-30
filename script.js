ec91aaffcb9cd6bd10d1c1e14750b26c9613c498
jaspreetScripts <119395932+jaspreetScripts@users.noreply.github.com>
2023-06-30 13:23:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+const form = document.querySelector('form');
+const inputText= document.getElementById('text');
+const inputDelay =  document.getElementById('Delay')
+const outputDiv= document.getElementById('output')
+
+form.addEventListener('submit',async(event)=>{
+	event.preventDefault();
+
+	const text= inputText.value;
+	const delay  = inputDelay.value;
+
+	await displayTextAfterDelay(text,delay)
+});
+
+async displayTextAfterDelay(text,delay){
+	await sleep(delay)
+
+
+	outputDiv.textContent= text
+}
+
+function sleep(ms){
+		return new Promise(resolve=> setTimeout(resolve,ms));
+
+	
+}
+
+
+
+
